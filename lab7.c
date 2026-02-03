17bff8319f0a5bb5b19257d08d8658aaf92fb626
พิพัฒน์ โชคโคกกรวด <s6804062612277@email.kmutnb.ac.th>
2026-02-04 01:51:31 +0700

    Commit 6>> Lab7no6.c: To initialize array2d without same number

--- lab7.c
+++ lab7.c
@@ -1,15 +1,41 @@
 #include <stdio.h>
-long convert(long h,long m,long s);
+#include <stdlib.h>
+#include <time.h>
+int random100to200();
 int main(){
-    int hour,minutes,seconds;
-    long avs;
-    scanf("%d %d %d",&hour,&minutes,&seconds);
-    avs = convert(hour,minutes,seconds);
-    printf("seconds = %d",avs);
+    int arrayy[20][20],m,n,t=1,stf;
+    srand(time(NULL));
+    scanf("%d %d",&m,&n);
+    if ((m <= 2 || m >20)||(n <= 2|| n > 20)){
+        return 0;
+    }
+    for(int i =0;i<m;i++){
+        for(int j=0;j<n;j++){
+            t = 1;
+            while(t){
+                stf = random100to200();
+                t = 0;
+                for(int c =0;c<m;c++){
+                    for(int y=0;y<n;y++){
+                        if(stf == arrayy[c][y]){
+                            t = 1;
+                        }
+                    }
+                }
+            }
+            arrayy[i][j] = stf;
+        }
+    }
+    for(int i = 0;i<m;i++){
+        for(int j = 0;j<n;j++){
+            printf("%d ",arrayy[i][j]);
+        }
+        printf("\n");
+    }
     return 0;
 }
-long convert(long h,long m,long s){
-    long aveee;
-    aveee = (h*60*60) + (m * 60) + s;
-    return aveee;
+int random100to200(){
+    int ran;
+    ran = (rand() % 101)+100;
+    return ran;
 }
